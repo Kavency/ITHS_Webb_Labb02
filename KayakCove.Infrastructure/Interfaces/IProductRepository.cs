abe9ea3eed04f410646ce091dbc949546ee1f275
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-11 19:13:45 +0100

    Add IProductRepository

---
+++ KayakCove.Infrastructure/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using KayakCove.Domain.Entities;
+
+namespace KayakCove.Infrastructure.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductById(int id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
