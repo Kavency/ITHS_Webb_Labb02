92552f5709e2eb40e78c48a6bd7d3a2af9fabd47
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-11 14:47:22 +0100

    Update product class with an image property.

--- KayakCove.Domain/Entities/Product.cs
+++ KayakCove.Domain/Entities/Product.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string ImageUri { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public Category? Category { get; set; }
         public bool IsInStock { get; set; }
