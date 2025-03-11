744e231099d0d3ffe66a6ab86b39b85ae469f9e8
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-11 20:51:34 +0100

    Add products controller

---
+++ KayakCove.Web/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using KayakCove.Application.Services;
+using KayakCove.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KayakCove.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly ProductService _productService;
+
+    public ProductsController(ProductService productService)
+    {
+        this._productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        var products = await _productService.GetAllProductsAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProduct(int id)
+    {
+        var product = await _productService.GetProductByIdAsync(id);
+        return product == null ? NotFound() : Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] Product product)
+    {
+        await _productService.AddProductAsync(product);
+        return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(int id, [FromBody] Product product)
+    {
+        if (id != product.Id) return BadRequest();
+        await _productService.UpdateProductAsync(product);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        await _productService.DeleteProductAsync(id);
+        return NoContent();
+    }
+}
