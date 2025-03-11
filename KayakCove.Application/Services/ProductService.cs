a9a17478fc2d3327b0fed62c917c5de159fedbae
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-11 19:51:20 +0100

    Add ProductService

---
+++ KayakCove.Application/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using KayakCove.Domain.Entities;
+using KayakCove.Infrastructure.Interfaces;
+
+namespace KayakCove.Application.Services;
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        this._productRepository = productRepository;
+    }
+
+    public Task<IEnumerable<Product>> GetAllProductsAsync() => _productRepository.GetAllProductsAsync();
+    public Task<Product> GetProductByIdAsync(int id) => _productRepository.GetProductByIdAsync(id);
+    public Task AddProductAsync(Product product) => _productRepository.AddProductAsync(product);
+    public Task UpdateProductAsync(Product product) => _productRepository.UpdateProductAsync(product);
+    public Task DeleteProductAsync(int id) => _productRepository.DeleteProductAsync(id);
+}
