14c1e57289d9de5a331ad8c92d6e215b48e4d799
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-11 14:43:04 +0100

    Add domain project and product, category classes

---
+++ KayakCove.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace KayakCove.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public Category? Category { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
