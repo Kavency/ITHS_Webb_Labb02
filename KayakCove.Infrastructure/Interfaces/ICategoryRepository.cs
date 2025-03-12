c2c9e3b7e53f462afb007f7fa60a514930cedefc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-12 15:03:50 +0100

    Add Category interface

---
+++ KayakCove.Infrastructure/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+using KayakCove.Domain.Entities;
+
+namespace KayakCove.Infrastructure.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    Task<Category> GetCategoryByIdAsync(int id);
+    Task AddCategoryAsync(Category category);
+    Task UpdateCategoryAsync(Category category);
+    Task DeleteCategoryAsync(int id);
+}
