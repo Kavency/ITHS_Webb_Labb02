d5966ce59e448c4c0892b888680d48237f511f97
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-12 15:04:04 +0100

    Add Category repository class

---
+++ KayakCove.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+using KayakCove.Domain.Entities;
+using KayakCove.Infrastructure.Data;
+using KayakCove.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KayakCove.Infrastructure.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryRepository(ApplicationDbContext context)
+    {
+        this._context = context;
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category> GetCategoryByIdAsync(int id)
+    {
+        return await _context.Categories.FindAsync(id);
+    }
+    
+    public async Task AddCategoryAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAsync(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+    }
+}
