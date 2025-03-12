1158b46ae31a21ee22bd9da2f3d8125c6c348f31
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-12 15:03:35 +0100

    Add Category service

---
+++ KayakCove.Application/Services/CategoryService.cs
@@ -0,0 +1,20 @@
+using KayakCove.Domain.Entities;
+using KayakCove.Infrastructure.Interfaces;
+
+namespace KayakCove.Application.Services;
+
+public class CategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        this._categoryRepository = categoryRepository;
+    }
+
+    public Task<IEnumerable<Category>> GetAllCategoriesAsync() => _categoryRepository.GetAllCategoriesAsync();
+    public Task<Category> GetCategoryByIdAsync(int id) => _categoryRepository.GetCategoryByIdAsync(id);
+    public Task AddCategoryAsync(Category category) => _categoryRepository.AddCategoryAsync(category);
+    public Task UpdateCategoryAsync(Category category) => _categoryRepository.UpdateCategoryAsync(category);
+    public Task DeleteCategoryAsync(int id) => _categoryRepository.DeleteCategoryAsync(id);
+}
