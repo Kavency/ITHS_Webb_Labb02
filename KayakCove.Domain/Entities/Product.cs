4188be791294d10d7c5888c503cfca39737d3138
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-12 07:48:16 +0100

    Refactor product class

--- KayakCove.Domain/Entities/Product.cs
+++ KayakCove.Domain/Entities/Product.cs
@@ -7,7 +7,9 @@
         public string Description { get; set; } = string.Empty;
         public string ImageUri { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public Category? Category { get; set; }
         public bool IsInStock { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
--- KayakCove.Infrastructure/Migrations/20250312061423_Refactor_Product_Entity.Designer.cs
+++ KayakCove.Infrastructure/Migrations/20250312061423_Refactor_Product_Entity.Designer.cs
@@ -3,6 +3,7 @@ using KayakCove.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KayakCove.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250312061423_Refactor_Product_Entity")]
+    partial class Refactor_Product_Entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
