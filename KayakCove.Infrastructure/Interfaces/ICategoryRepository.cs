13204aacfae51b276ce0b2ceb5e272f28a484518
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-14 18:00:40 +0100

    Update class name

--- KayakCove.Infrastructure/Interfaces/ICategoryRepository.cs
+++ KayakCove.Infrastructure/Interfaces/ICategoryRepository.cs
@@ -6,7 +6,7 @@ public interface ICategoryRepository
 {
     Task<IEnumerable<Category>> GetAllCategoriesAsync();
     Task<Category> GetCategoryByIdAsync(int id);
-    Task AddCategoryAsync(Category category);
+    Task CreateCategoryAsync(Category category);
     Task UpdateCategoryAsync(Category category);
     Task DeleteCategoryAsync(int id);
 }
