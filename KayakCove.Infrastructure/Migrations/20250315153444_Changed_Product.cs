5eed39acf5d1779a22d5128be4bc62b0fcadf888
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-15 16:37:53 +0100

    Add migration and update database with updated product

---
+++ KayakCove.Infrastructure/Migrations/20250315153444_Changed_Product.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KayakCove.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changed_Product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsInStock",
+                table: "Products",
+                newName: "HasExpired");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "HasExpired",
+                table: "Products",
+                newName: "IsInStock");
+        }
+    }
+}
