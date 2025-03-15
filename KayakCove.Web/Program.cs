951eefec4138136b51382b69d1f7200361933d07
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-15 15:08:07 +0100

    Add Http Client service

--- KayakCove.Web/Program.cs
+++ KayakCove.Web/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddHttpClient("ApiClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7213/");
+});
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
