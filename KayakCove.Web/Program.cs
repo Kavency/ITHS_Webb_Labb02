72d2efe15ee0deed36b7f6f9456fb0939869babe
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-16 20:52:24 +0100

    Move app services to API layer

--- KayakCove.Web/Program.cs
+++ KayakCove.Web/Program.cs
@@ -1,34 +1,11 @@
 using KayakCove.Web.Components;
-using KayakCove.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-using KayakCove.Infrastructure.Interfaces;
-using KayakCove.Infrastructure.Repositories;
-using KayakCove.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddHttpClient("ApiClient", client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7213/");
-});
-
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConnection")));
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<CategoryService>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-});
 
 var app = builder.Build();
 
@@ -44,13 +21,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
 app.MapControllers();
 
 app.UseStaticFiles();
-app.UseCors("AllowAll");
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
