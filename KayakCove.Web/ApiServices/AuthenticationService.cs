37d964da830c8434f47d5056d15c183e8aaba381
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-30 21:21:45 +0200

    Add auth service with jsinterop

---
+++ KayakCove.Web/ApiServices/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.JSInterop;
+
+namespace KayakCove.Web.ApiServices;
+
+public class AuthenticationService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public AuthenticationService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task<bool> IsLoggedInAsync()
+    {
+        var result = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "isLoggedIn");
+        return result == "true";
+    }
+
+    public async Task LoginAsync(int id, string username)
+    {
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "isLoggedIn", "true");
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "id", id);
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "username", username);
+    }
+
+    public async Task LogoutAsync()
+    {
+        await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "isLoggedIn");
+        await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "id");
+        await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "username");
+    }
+}
