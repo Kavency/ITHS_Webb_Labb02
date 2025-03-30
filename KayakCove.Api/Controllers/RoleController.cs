7280dc0b598a19135e7862a91e1ec12d169bb47f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-30 19:50:17 +0200

    Add role controller

---
+++ KayakCove.Api/Controllers/RoleController.cs
@@ -0,0 +1,30 @@
+﻿using KayakCove.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KayakCove.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
+{
+    private readonly RoleService _roleService;
+
+    public RoleController(RoleService roleService)
+    {
+        this._roleService = roleService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRoles()
+    {
+        var roleDtos = await _roleService.GetAllRolesAsync();
+        return Ok(roleDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetRoleById(int id)
+    {
+        var roleDto = await _roleService.GetRoleByIdAsync(id);
+        return roleDto == null ? NotFound() : Ok(roleDto);
+    }
+}
