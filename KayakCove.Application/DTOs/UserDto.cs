a3ffd8eff0b34e992634a69a0c9286697e15e849
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-30 19:49:18 +0200

    Add user Dto

---
+++ KayakCove.Application/DTOs/UserDto.cs
@@ -0,0 +1,42 @@
+﻿using KayakCove.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace KayakCove.Application.DTOs;
+
+public class UserDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    [Required]
+    public string Firstname { get; set; }
+
+    [Required]
+    public string Lastname { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+    
+    public string Phonenumber { get; set; }
+    
+    public string Streetaddress { get; set; }
+    
+    public string Postalcode { get; set; }
+    
+    public string City { get; set; }
+    
+    public string Country { get; set; }
+    public DateOnly AccountCreated { get; set; }
+    public DateOnly LastLogin { get; set; }
+    public DateOnly LastPasswordChange { get; set; }
+
+    [Required]
+    public int RoleId { get; set; }
+
+    public Role Role { get; set; }
+}
