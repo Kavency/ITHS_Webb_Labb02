0f80da49b280f346229b0484770a87b1b48b7ef3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-30 19:49:48 +0200

    Add user repo interface

---
+++ KayakCove.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using KayakCove.Domain.Entities;
+
+namespace KayakCove.Infrastructure.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User> GetUserByIdAsync(int id);
+    Task<bool> CreateUserAsync(User user);
+    Task<bool> UpdateUserAsync(User user);
+    Task<bool> DeleteUserAsync(int id);
+}
