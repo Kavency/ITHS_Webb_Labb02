9e4a8e22d28fe8c269bad251b8f282e57955892d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-02 14:09:00 +0200

    Add CartItem Dto

---
+++ KayakCove.Application/DTOs/CartItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KayakCove.Application.DTOs;
+
+class CartItemDto
+{
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+    public ProductDto ProductDto { get; set; }
+    public int Amount { get; set; }
+}
