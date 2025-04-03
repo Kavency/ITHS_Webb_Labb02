352057ff76ebed664f581250388b2106ac9fecb6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-03 20:54:30 +0200

    #44 Add interface for OrderDetails

---
+++ KayakCove.Infrastructure/Interfaces/IOrderDetailsRepository.cs
@@ -0,0 +1,12 @@
+﻿using KayakCove.Domain.Entities;
+
+namespace KayakCove.Infrastructure.Interfaces;
+
+interface IOrderDetailsRepository
+{
+    Task<IEnumerable<OrderDetails>> GetAllOrdersAsync();
+    Task<OrderDetails> GetOrderByIdAsync(int id);
+    Task<OrderDetails> CreateOrderAsync(OrderDetails orderDetails);
+    Task<OrderDetails> UpdateOrderAsync(OrderDetails orderDetails);
+    Task<bool> DeleteOrderAsync(int id);
+}
