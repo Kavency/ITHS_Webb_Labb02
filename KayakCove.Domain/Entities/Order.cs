66d0f2a675f7ccc23fc99f2a992767e4414e0610
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-03 15:32:42 +0200

    #32 Add Order entity

---
+++ KayakCove.Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace KayakCove.Domain.Entities;
+
+class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string OrderStatus { get; set; }
+    public decimal GrandTotal { get; set; }
+}
