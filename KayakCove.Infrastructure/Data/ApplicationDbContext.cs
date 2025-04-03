3f316f1f9f356c1acf0b49c2f9d154a29477b779
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-03 16:00:50 +0200

    #42 Migrate the new classes to the DataBase

--- KayakCove.Infrastructure/Data/ApplicationDbContext.cs
+++ KayakCove.Infrastructure/Data/ApplicationDbContext.cs
@@ -11,5 +11,7 @@ namespace KayakCove.Infrastructure.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
