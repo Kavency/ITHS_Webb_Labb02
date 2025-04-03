32a609684e4e72a8d570a5cc05b6f53e58ec587e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-03 16:15:52 +0200

    #44 Add order interface

---
+++ KayakCove.Infrastructure/Interfaces/IOrder.cs
@@ -0,0 +1,12 @@
+﻿using KayakCove.Domain.Entities;
+
+namespace KayakCove.Infrastructure.Interfaces;
+
+interface IOrder
+{
+    Task<IEnumerable<Category>> GetAllOrdersAsync();
+    Task<Category> GetOrderByIdAsync(int id);
+    Task CreateOrderAsync(Category category);
+    Task UpdateOrderAsync(Category category);
+    Task<bool> DeleteOrderAsync(int id);
+}
