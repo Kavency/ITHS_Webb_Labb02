3f69400665913c50e9dda1016e4e1921acfa0dea
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-04-05 19:46:28 +0200

    Fix search on product page

--- KayakCove.Application/DTOs/CartItemDto.cs
+++ KayakCove.Application/DTOs/CartItemDto.cs
@@ -2,7 +2,6 @@
 
 public class CartItemDto
 {
-    public int UserId { get; set; }
     public int ProductId { get; set; }
     public ProductDto ProductDto { get; set; }
     public int Amount { get; set; }
